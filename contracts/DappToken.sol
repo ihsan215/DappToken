9a359afbfedc519264deb50c3c60984706a84371
ihsan215 <aliihsantas34@gmail.com>
2023-09-20 14:47:42 +0300

    edited DappToken Contract

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,15 +1,22 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.4.2;
 
 
 contract DappToken{
 
+    string public name = "Dapp Token";
+    string public symbol = "DAPTKN";
+    string public standard = "Dapp Token v0.1";
     uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
 
-    constructor() public {
-        totalSupply = 1000000;
-
+    constructor(uint256 _initialSupply) public {
+        totalSupply = _initialSupply;
+        balanceOf[msg.sender] = totalSupply;
     }
+
+    
+
+
 
 }
--- test/DappToken.test.js
+++ test/DappToken.test.js
@@ -1,13 +1,37 @@
 const DappToken = artifacts.require("DappToken");
+const TOKEN_STANDART = "Dapp Token v0.1";
 
-contract("DappToken", function (accounts) {
-  it("sets the total supply upon deployment", async function () {
-    const DappTokenContract = await DappToken.deployed();
-    const totalSupply = await DappTokenContract.totalSupply();
-    assert.equal(
-      totalSupply.toNumber(),
-      1000000,
-      "sets the total supply to 1,000,000"
-    );
+contract("DappToken", async function (accounts) {
+  describe("DappToken Test", function () {
+    let DappTokenContract;
+    before(async function () {
+      DappTokenContract = await DappToken.deployed();
+    });
+
+    it("check token standart", async () => {
+      const tokenStandard = await DappTokenContract.standard();
+      assert.equal(tokenStandard, TOKEN_STANDART, "correct standart");
+    });
+
+    it("sets the total supply upon deployment", async function () {
+      const totalSupply = await DappTokenContract.totalSupply();
+      assert.equal(
+        totalSupply.toNumber(),
+        1000000,
+        "sets the total supply to 1,000,000"
+      );
+    });
+
+    it("check admin balance", async () => {
+      const adminBalance = await DappTokenContract.balanceOf(accounts[0]);
+      assert.equal(adminBalance.toNumber(), 1000000, "check admin balance");
+    });
+
+    it("check name and symbol", async () => {
+      const name = await DappTokenContract.name();
+      const symbol = await DappTokenContract.symbol();
+      assert.equal(name, "Dapp Token", "correct name");
+      assert.equal(symbol, "DAPTKN", "correct symbol");
+    });
   });
 });
